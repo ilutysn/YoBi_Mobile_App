fc74a93966fc4b682ea23783a53f154b1de13891
kikirizki <kiki@rizki.com>
2020-11-02 20:14:37 +0700

    Add register page

--- app/src/main/java/com/tugas/yobimobile/MainActivity.java
+++ app/src/main/java/com/tugas/yobimobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tugas.yobimobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,5 +23,9 @@ public class MainActivity extends AppCompatActivity {
     }
     public void register(View view) {
 //        Fungsi untuk mentrigger activity baru
+        Intent intent = new Intent(this, RegisterActivity.class);
+
+        startActivity(intent);
+
     }
 }
