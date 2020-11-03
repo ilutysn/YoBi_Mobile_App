e29645a9bd854b294e18c768c329f3833745ed4c
kikirizki <kiki@rizki.com>
2020-11-03 14:45:18 +0700

    Add yobi logo so splash screen

--- app/src/main/java/com/tugas/yobimobile/SplashActivity.java
+++ app/src/main/java/com/tugas/yobimobile/SplashActivity.java
@@ -5,23 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
-    Handler handler;
+    Handler handler_for_mainmenu;
+    Handler handler_for_logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        ImageView imageAnim = (ImageView) findViewById(R.id.splash_logo);
+        handler_for_mainmenu = new Handler();
+        handler_for_logo = new Handler();
+        handler_for_logo.postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        imageAnim.setVisibility(1);
+                    }
+                }, 800
 
-        handler=new Handler();
-        handler.postDelayed(new Runnable() {
+        );
+
+
+        handler_for_mainmenu.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        }, 3000);
 
     }
 }
