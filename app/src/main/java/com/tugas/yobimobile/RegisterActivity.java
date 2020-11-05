602b12424c7a1b5094ad8a7e8bfa7cd7f1de9ff0
kikirizki <kiki@rizki.com>
2020-11-05 17:33:11 +0700

    Add back button functionality

--- app/src/main/java/com/tugas/yobimobile/RegisterActivity.java
+++ app/src/main/java/com/tugas/yobimobile/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.tugas.yobimobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
+    }
+
+    public void back_home(View view) {
+        Intent login_intent = new Intent(this, LoginActivity.class);
+        startActivity(login_intent);
+
     }
 }
