b7c5177e229abadb70f3bc858c556cbf4352a8c6
kikirizki <kiki@rizki.com>
2020-11-14 17:46:23 +0700

    Update register activity

--- app/src/main/java/com/tugas/yobimobile/RegisterActivity.java
+++ app/src/main/java/com/tugas/yobimobile/RegisterActivity.java
@@ -1,18 +1,22 @@
 package com.tugas.yobimobile;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-;
+;import com.tugas.yobimobile.database.UserRegisterDbHelper;
+
 public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
+        UserRegisterDbHelper dbHelper = new UserRegisterDbHelper(getApplicationContext());
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
     }
 
     public void back_home(View view) {
